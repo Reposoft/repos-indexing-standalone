e2fc6e41c56a9a4f58bb9f651aee8103d238e46c
Jonathan Andersson <jonathan.andersson@simonsoft.se>
2017-10-10 08:53:05 +0200

    jax-rs client could not be initialized with a Ssefeature due to problem with dependencies. The Cxf Client package includes jars thats already is included. Added test that display if repos-indexing-standalone may init a Client.

---
+++ src/test/java/se/repos/indexing/standalone/IndexingDaemonPubSubTest.java
@@ -0,0 +1,16 @@
+package se.repos.indexing.standalone;
+
+import javax.ws.rs.client.ClientBuilder;
+
+import org.glassfish.jersey.media.sse.SseFeature;
+import org.junit.Test;
+
+public class IndexingDaemonPubSubTest {
+	
+	@Test
+	public void testBuildClient() {		
+		//Problems with dependencies of jax.rs.ws causes the SseFeature to crash on init. this test ensures that it is possible for this module to init a client with SseFeature.
+		ClientBuilder.newBuilder().register(SseFeature.class).build();
+	}
+
+}
