39997e8c38b7718a4d888bc916c6e7e666e35e3f
Thomas Åkesson <thomas.akesson@simonsoft.se>
2024-12-12 00:18:35 +0100

    Injection works, attempts connection to svn.

--- src/main/java/se/repos/indexing/standalone/IndexingLambda.java
+++ src/main/java/se/repos/indexing/standalone/IndexingLambda.java
@@ -7,6 +7,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Module;
+import com.google.inject.ProvisionException;
 import com.google.inject.name.Names;
 
 import se.repos.indexing.ReposIndexing;
@@ -18,24 +19,35 @@ import se.repos.indexing.standalone.config.ParentModule;
 import se.repos.indexing.standalone.config.SolrCoreProvider;
 import se.repos.indexing.standalone.config.SolrCoreProviderAssumeExisting;
 import se.simonsoft.cms.backend.svnkit.CmsRepositorySvn;
+import se.simonsoft.cms.item.CmsRepository;
+import se.simonsoft.cms.item.RepoRevision;
 import se.simonsoft.cms.item.info.CmsRepositoryLookup;
 import se.simonsoft.cms.item.inspection.CmsContentsReader;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IndexingLambda implements RequestHandler<List<Integer>, Integer> {
 
 	private static Boolean inited = init();
 
+	private static final Logger logger = LoggerFactory.getLogger(IndexingLambda.class);
+			
 	// Current cms-indexing-lambda has env: CMS_CLOUDID, CMS_SOLR_URL 
 	// Likely add CMS_SVN_URL 
 	
 	private static String solrUrl = "http://localhost:8983/solr/";
-	private static String svnUrl = "http://localhost:8983/svn/demo1";
+	// CmsRepository may not specify a custom port (provider uses 8091)
+	private static String svnUrl = "http://localhost/svn/demo1"; 
 	
+	private static CmsRepositorySvn repo = new CmsRepositorySvn(svnUrl);
 	private static SolrCoreProvider solrCoreProvider = new SolrCoreProviderAssumeExisting(solrUrl);
 	private static Injector global = getGlobal(solrCoreProvider);
-	private static Injector context = getSvn(global, new CmsRepositorySvn(svnUrl));
+	private static Injector context = getSvn(global, repo);
 	private static ReposIndexing indexing = context.getInstance(ReposIndexing.class);
 	private static CmsRepositoryLookup lookup = context.getInstance(Key.get(CmsRepositoryLookup.class, Names.named("inspection")));
 	private static CmsContentsReader contents = context.getInstance(CmsContentsReader.class);
@@ -64,13 +76,70 @@ public class IndexingLambda implements RequestHandler<List<Integer>, Integer> {
 		System.out.println("Indexing... " + lookup);
 		System.out.println("Indexing... " + contents);
 
-		
-		
+		// TODO: https://stackoverflow.com/questions/46080448/logging-in-aws-lambda-with-slf4j
+		/*
 		LambdaLogger logger = context.getLogger();
 		logger.log("EVENT TYPE: " + event.getClass().toString());
+		*/
+		
+		logger.info("Starting sync for repository '{}'", repo.getName());
+		
+		System.out.println("Starting sync for repository: " + repo.getName());
+		try {
+			runOnce(lookup, repo);
+
+		} catch (ProvisionException e) {
+			// Workaround for bug in Guice.
+			Field field = null;
+			Set<?> value = Set.of("");
+			try {
+				field = e.getClass().getDeclaredField("messages");
+				field.setAccessible(true);
+				value = (Set<?>) field.get(e);
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}    
+			System.out.println("Dependency injection failed: " + value.iterator().next());
+			
+		} catch (Exception e) {
+			System.out.println("Failed: " + e.getClass());
+			logger.error("Sync failed for repository '{}'", repo.getName(), e);
+			throw e;
+		} catch (Error error) {
+			// Catch errors, like OutOfMemoryError and classloader issues.
+			// Ensure that the JVM terminates instead of starting the next Callable in queue.
+			try {
+				logger.error("Sync failed with Error for repository '{}'", repo.getName(), error);
+			} finally {
+				System.exit(-10);
+			}
+			//throw error;
+		}
+
+		
 		return event.stream().mapToInt(Integer::intValue).sum();
 	}
 	
+	/**
+	 * Run single repository
+	 * @param lookup
+	 * @param repo
+	 */
+	protected boolean runOnce(CmsRepositoryLookup lookup, CmsRepository repo) {
+		RepoRevision head;
+		try {
+			head = lookup.getYoungest(repo);
+		} catch (RuntimeException e) {
+			logger.warn("Repository {} not found", repo.getName());
+			//return false;
+			throw e;
+		}
+
+		logger.debug("Sync {} {}", repo, head);
+		indexing.sync(head);
+		return true;
+	}
 	
 	
 	private static Boolean init() {
---
+++ src/test/java/se/repos/indexing/standalone/IndexingLambdaTest.java
@@ -0,0 +1,18 @@
+package se.repos.indexing.standalone;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+public class IndexingLambdaTest {
+
+	@Test @Ignore
+	public void test() {
+		IndexingLambda lambda = new IndexingLambda();
+		lambda.handleRequest(null, null);
+		//fail("Not yet implemented");
+	}
+
+}
