6dfb091ce71e5a97cac3c369739c3fda7a19872d
Thomas Åkesson <thomas.akesson@simonsoft.se>
2024-12-14 01:24:54 +0100

    Test command with minimal SQSEvent.

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ export DOCKER_HOST=unix:///Users/takesson/.docker/run/docker.sock
 sam local start-lambda --debug
 
 # Second terminal
-awslocal lambda invoke --function-name HelloLambdaFunction --endpoint-url=http://127.0.0.1:3001 --cli-binary-format raw-in-base64-out --payload '[1,4,7]' output.txt
+awslocal lambda invoke --function-name HelloLambdaFunction --endpoint-url=http://127.0.0.1:3001 --cli-binary-format raw-in-base64-out --payload '{"Records":[]}' output.txt
